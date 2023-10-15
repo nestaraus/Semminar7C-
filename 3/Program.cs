9d9b817146f11cfc6992b5a40a5cf604ea0776b1
nestaraus <nestaraus@mail.pu>
2023-10-15 20:31:05 +0300

    zadacha 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле Anm=m+n
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, и замените эти элементы на их квадраты
 
 void InputMatrix(int[,] matrix)
 {
@@ -6,7 +6,7 @@ void InputMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = i+j;
+            matrix[i,j] =new Random().Next(-10,11);
         }
     }
 }
@@ -23,6 +23,20 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+void ReleaseMatrix(int[,] matrix)
+{
+            for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i%2==1&&j%2==1)
+            matrix[i,j]*=matrix[i,j];
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
 
 Console.Clear();
 Console.Write("Введите размер матрицы: ");
@@ -30,3 +44,5 @@ int[] size = Console.ReadLine().Split(" ").Select(x=> int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
