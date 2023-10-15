ca419762742607a6a7b8334fda5b350760c463fa
nestaraus <nestaraus@mail.pu>
2023-10-15 20:43:12 +0300

    zadacha 4

---
+++ 4/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали с индексами (0,0); (1,1) и тд
+
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, и замените эти элементы на их квадраты
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] =new Random().Next(-10,11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix) 
+{
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i==j) 
+            sum += matrix[i,j];
+        }
+        Console.WriteLine();
+    }
+    Console.Write(sum);
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x=> int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
